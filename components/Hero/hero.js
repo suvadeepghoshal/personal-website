f5964d83d3a82c58b36da71ea0b5163866db8f22
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 20:19:15 +0530

    basic content for the hero

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -7,13 +7,12 @@ const Hero = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Hello There!</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            I&apos;m a full stack software enthusiast and algorithm researcher
+            based in India!
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Read More</button>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function Home() {
   }, []);
   return (
     <Section>
-      <div>Home Page</div>
       <Hero />
       <Feature />
     </Section>
