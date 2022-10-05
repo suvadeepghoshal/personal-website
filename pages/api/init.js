8e12c4eaeaf679e44faa63d2b16c20e649ce0016
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 19:21:58 +0530

    sending the HTTP response instead of JSON

--- pages/api/init.js
+++ pages/api/init.js
@@ -3,24 +3,31 @@
 const { Navigator } = require('node-navigator');
 
 const initApi = (req, res) => {
-  const D = new Date();
-  const navigator = new Navigator();
-  res.status(200).json({
-    date: `${D.getFullYear().toString()}/${D.getMonth().toString()}/${D.getDay().toString()}`,
-    time: `${D.getHours().toString()}:${D.getMinutes().toString()}:${D.getSeconds().toString()}:${D.getMilliseconds().toString()}`,
-    navigator: {
-      appVersion: navigator.appVersion,
-      appName: navigator.appName,
-      appCodeName: navigator.appCodeName,
-      geoLocation: navigator.geolocation,
-      langugaes: navigator.languages,
-      online: navigator.onLine,
-      pltform: navigator.platform,
-      product: navigator.product,
-      vendor: navigator.vendor,
-      userAgent: navigator.userAgent
-    }
-  });
+  if (req.method === 'GET') {
+    const D = new Date();
+    const navigator = new Navigator();
+    res.status(200).send({
+      date: `${D.getFullYear().toString()}/${D.getMonth().toString()}/${D.getDay().toString()}`,
+      time: `${D.getHours().toString()}:${D.getMinutes().toString()}:${D.getSeconds().toString()}:${D.getMilliseconds().toString()}`,
+      navigator: {
+        appVersion: navigator.appVersion,
+        appName: navigator.appName,
+        appCodeName: navigator.appCodeName,
+        geoLocation: navigator.geolocation,
+        langugaes: navigator.languages,
+        online: navigator.onLine,
+        pltform: navigator.platform,
+        product: navigator.product,
+        vendor: navigator.vendor,
+        userAgent: navigator.userAgent
+      }
+    });
+  } else {
+    res.status(500).send({
+      errorCode: 500,
+      errorMessage: 'Not a valid method for the API'
+    });
+  }
 };
 
 export default initApi;
