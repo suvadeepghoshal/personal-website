c959252190f1d8d91c6f3b84938a97f4593e8fb0
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 19:42:08 +0530

    init api call on first time session undefinity

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import { useEffect } from 'react';
 import Feature from '../components/Feature/feature';
 import Hero from '../components/Hero/hero';
 import Section from '../components/Layout/section';
 import StorageService from '../services/storageService';
 
 export default function Home() {
-  const { setItem } = StorageService();
-  setItem('init', 'init', 'session');
+  const { setItem, getItem } = StorageService();
+  useEffect(() => {
+    const existingInitData = getItem('init', 'session');
+    if (existingInitData === undefined) {
+      fetch('/api/init')
+        .then((response) => {
+          if (!response.ok) throw Error(response.status);
+          else return response.json();
+        })
+        .then((data) => {
+          setItem('init', data, 'session');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, []);
   return (
     <Section>
       <div>Home Page</div>
