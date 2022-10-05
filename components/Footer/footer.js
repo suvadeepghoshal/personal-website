d76ef53ef1ce92fc1a898097183af631e59b48a1
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 20:06:29 +0530

    footer ui designed

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,8 +1,13 @@
 const Footer = () => {
   return (
-    <div>
-      &copy; {new Date().getFullYear()} Suvadeep Ghoshal. All rights reserved.
-    </div>
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>
+          &copy; {new Date().getFullYear()} Suvadeep Ghoshal. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
   );
 };
 export default Footer;
