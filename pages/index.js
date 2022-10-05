3d2a877fc8146d6c67b065ae738fa1efc83db79d
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 11:23:29 +0530

    layout of the home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Section from '../components/Layout/section';
 export default function Home() {
   return (
     <Section>
-      <div>This is the home page</div>
+      <div>Home Page</div>
+      <ul>
+        <li>Hero Component</li>
+        <li>Features/Advertise component</li>
+      </ul>
     </Section>
   );
 }
