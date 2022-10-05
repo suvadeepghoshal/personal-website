a60da70be8d4cde15533cbf5627e05db742079a0
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 19:21:27 +0530

    added eslint config for prettier plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended'
+  ],
   overrides: [],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
-    indent: ['error', 'space'],
+    // indent: ['error', 'space'],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-  },
+    'prettier/prettier': 'error'
+  }
 };
