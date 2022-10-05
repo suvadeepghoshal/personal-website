1680e3ce4f496fe3ae8dd47fecc2d7b786e043bc
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-05 09:35:51 +0530

    no trailing commas

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: 'none',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  singleAttributePerLine: true,
+  singleAttributePerLine: true
 };
