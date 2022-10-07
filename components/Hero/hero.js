8c0a3878b25ed72fc97dd730c7d7a0746a5c2b57
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-10-07 08:59:39 +0530

    eslint correction

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div
       className="hero min-h-screen"
-      style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
+      style={{ backgroundImage: 'url("https://placeimg.com/1000/800/arch")' }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
